a818af9813ffc011cf222af68f597a4705e2457a
Alex <alexwyllie4.gmail.com>
2026-04-29 14:21:02 +0100

    EDIT: Multiple errors fixed all over the shop

--- test_bye.py
+++ test_bye.py
@@ -1,10 +1,10 @@
 from application import app
 
 
-def test_hello_page():
+def test_bye_page():
     with app.test_client() as test_client:
-        response = test_client.get('/hello')
+        response = test_client.get('/bye')
         assert response.status_code == 200
-        assert b"Hello World" in response.data
-       
+        assert b"Goodbye World" in response.data
+        assert b"Alex" in response.data
         assert b"Version" in response.data
